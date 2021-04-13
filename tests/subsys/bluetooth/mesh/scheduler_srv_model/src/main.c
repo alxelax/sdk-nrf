3e22c80aa948a7cf78745df635dca8bab324b45d
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2021-04-13 14:17:36 +0200

    Bluetooth: Mesh: scheduler initialization unit test
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- tests/subsys/bluetooth/mesh/scheduler_srv_model/src/main.c
+++ tests/subsys/bluetooth/mesh/scheduler_srv_model/src/main.c
@@ -8,10 +8,15 @@
 #include <bluetooth/mesh.h>
 #include <bluetooth/mesh/models.h>
 
+static void scheduler_action_set_cb(struct bt_mesh_scheduler_srv *srv,
+				    struct bt_mesh_msg_ctx *ctx,
+				    uint8_t idx,
+				    struct bt_mesh_schedule_entry *entry);
+
 static struct bt_mesh_time_srv time_srv = BT_MESH_TIME_SRV_INIT(NULL);
 static struct bt_mesh_scene_srv scene_srv;
 static struct bt_mesh_scheduler_srv scheduler_srv =
-	BT_MESH_SCHEDULER_SRV_INIT(NULL, &time_srv);
+	BT_MESH_SCHEDULER_SRV_INIT(scheduler_action_set_cb, &time_srv);
 
 static struct bt_mesh_elem elems[] = {
 	BT_MESH_ELEM(0, BT_MESH_MODEL_LIST(
@@ -26,26 +31,60 @@ static const struct bt_mesh_comp comp = {
 	.elem_count = ARRAY_SIZE(elems),
 };
 
-//static void test_setup(void)
-//{
-//	int err = 0;
-//	bt_mesh_model_foreach(mod_init, &err);
-//}
-//
-//static void test_teardown(void)
-//{
-//}
-#include "common/log.h"
-int bt_mesh_init(const struct bt_mesh_prov *prov,
-		 const struct bt_mesh_comp *comp)
+int bt_mesh_model_extend(struct bt_mesh_model *mod,
+			 struct bt_mesh_model *base_mod)
 {
-	BT_ERR("This is mock from test");
+
+	if (mod->id != BT_MESH_MODEL_ID_SCHEDULER_SRV)
+	{
+		return 0;
+	}
+
+	zassert_equal_ptr(scheduler_srv.model, mod,
+		"wrong model pointer for extension.");
+	zassert_equal_ptr(scheduler_srv.setup_mod, base_mod,
+		"wrong base model pointer for extension.");
+
 	return 0;
+}
+
+static void scheduler_action_set_cb(struct bt_mesh_scheduler_srv *srv,
+				    struct bt_mesh_msg_ctx *ctx,
+				    uint8_t idx,
+				    struct bt_mesh_schedule_entry *entry)
+{
 }
 
 static void test_scheduler_init(void)
 {
-	zassert_false(0 == bt_mesh_init(NULL, &comp), "Test fails");
+	zassert_true(0 == bt_mesh_init(NULL, &comp), "bt mesh initialization failed.");
+
+	zassert_equal_ptr(scheduler_srv.model, &elems[0].models[1],
+			"wrong initialization of the model pointer.");
+	zassert_equal_ptr(scheduler_srv.pub.msg, &scheduler_srv.pub_buf,
+			"wrong initialization of the publication message pointer.");
+	zassert_not_null(scheduler_srv.pub.update,
+			"update handler has not been initialized.");
+	zassert_equal_ptr(scheduler_srv.pub_buf.data, scheduler_srv.pub_data,
+			"wrong initialization of the network buffer.");
+	zassert_equal(scheduler_srv.pub_buf.len, ARRAY_SIZE(scheduler_srv.pub_data),
+			"wrong initialization of the network buffer length.");
+	zassert_equal(scheduler_srv.pub_buf.size, ARRAY_SIZE(scheduler_srv.pub_data),
+			"wrong initialization of the network buffer size.");
+	zassert_equal(scheduler_srv.status_bitmap, 0,
+			"scheduler status bitmap state has not been initialized.");
+	zassert_equal(scheduler_srv.idx, BT_MESH_SCHEDULER_ACTION_ENTRY_COUNT,
+				"scheduler register index has not been initialized.");
+
+	// todo scheduler register checking
+	// todo delayable work initialization checking
+
+	for (int i = 0; i < BT_MESH_SCHEDULER_ACTION_ENTRY_COUNT; i++) {
+		zassert_equal(scheduler_srv.sched_tai[i].sec, 0,
+			"scheduler tai sec not initialized instance: %i.", i);
+		zassert_equal(scheduler_srv.sched_tai[i].subsec, 0,
+			"scheduler tai subsec not initialized instance: %i.", i);
+	}
 }
 
 void test_main(void)
