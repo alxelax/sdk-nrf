2679a0be784fbe2bf2bd58e59f4d0891ba4a6801
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2021-04-29 18:44:43 +0200

    power down backend test

--- tests/subsys/bluetooth/mesh/scheduler_srv_model/src/main.c
+++ tests/subsys/bluetooth/mesh/scheduler_srv_model/src/main.c
@@ -5,6 +5,7 @@
  */
 
 #include <ztest.h>
+#include <bluetooth/bluetooth.h>
 #include <bluetooth/mesh.h>
 #include <bluetooth/mesh/models.h>
 
@@ -30,25 +31,6 @@ static const struct bt_mesh_comp comp = {
 };
 
 /****************** mock section **********************************/
-int bt_mesh_prov_init(const struct bt_mesh_prov *prov_info)
-{
-	return 0;
-}
-
-int bt_mesh_model_extend(struct bt_mesh_model *mod,
-			 struct bt_mesh_model *base_mod)
-{
-
-	if (mod->id != BT_MESH_MODEL_ID_SCHEDULER_SRV)
-	{
-		return 0;
-	}
-
-	zassert_equal_ptr(scheduler_srv.model, mod,
-		"wrong model pointer for extension.");
-
-	return 0;
-}
 /****************** mock section **********************************/
 
 /****************** callback section ******************************/
@@ -59,78 +41,79 @@ static void scheduler_action_set_cb(struct bt_mesh_scheduler_srv *srv,
 {
 }
 /****************** callback section ******************************/
+int bt_mesh_power_down_enter(void);
+int bt_mesh_power_down_leave(void);
 
-static void test_scheduler_init(void)
+static int output_number(bt_mesh_output_action_t action, uint32_t number)
 {
-	zassert_true(0 == bt_mesh_init(NULL, &comp), "bt mesh initialization failed.");
+	printk("OOB Number: %u\n", number);
 
-	zassert_equal_ptr(scheduler_srv.action_set_cb, scheduler_action_set_cb,
-			"wrong initialization of the action set callback.");
-	zassert_equal_ptr(scheduler_srv.time_srv, &time_srv,
-			"wrong initialization of the time server pointer.");
-
-	/* The first the scene\scene setup servers go. */
-	zassert_equal_ptr(scheduler_srv.model, &elems[0].models[2],
-			"wrong initialization of the model pointer.");
-	zassert_equal_ptr(scheduler_srv.pub.msg, &scheduler_srv.pub_buf,
-			"wrong initialization of the publication message pointer.");
-	zassert_not_null(scheduler_srv.pub.update,
-			"update handler has not been initialized.");
-	zassert_equal_ptr(scheduler_srv.pub_buf.data, scheduler_srv.pub_data,
-			"wrong initialization of the network buffer.");
-	zassert_equal(scheduler_srv.pub_buf.len, ARRAY_SIZE(scheduler_srv.pub_data),
-			"wrong initialization of the network buffer length.");
-	zassert_equal(scheduler_srv.pub_buf.size, ARRAY_SIZE(scheduler_srv.pub_data),
-			"wrong initialization of the network buffer size.");
-	zassert_equal(scheduler_srv.status_bitmap, 0,
-			"scheduler status bitmap state has not been initialized.");
-	zassert_equal(scheduler_srv.idx, BT_MESH_SCHEDULER_ACTION_ENTRY_COUNT,
-			"scheduler register index has not been initialized.");
-
-	for (int i = 0; i < BT_MESH_SCHEDULER_ACTION_ENTRY_COUNT; i++) {
-		zassert_equal(scheduler_srv.sch_reg[i].year, 0,
-			"scheduler register year not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].month, 0,
-			"scheduler register month not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].day, 0,
-			"scheduler register day not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].hour, 0,
-			"scheduler register hour not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].minute, 0,
-			"scheduler register minute not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].second, 0,
-			"scheduler register second not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].day_of_week, 0,
-			"scheduler register day_of_week not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].action, 0,
-			"scheduler register action not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].transition_time, 0,
-			"scheduler register transition_time not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sch_reg[i].scene_number, 0,
-			"scheduler register scene_number not initialized instance: %i.", i);
-	}
-
-	for (int i = 0; i < BT_MESH_SCHEDULER_ACTION_ENTRY_COUNT; i++) {
-		zassert_equal(scheduler_srv.sched_tai[i].sec, 0,
-			"scheduler tai sec not initialized instance: %i.", i);
-		zassert_equal(scheduler_srv.sched_tai[i].subsec, 0,
-			"scheduler tai subsec not initialized instance: %i.", i);
-	}
+	return 0;
 }
 
-int bt_mesh_power_down(void);
+static void prov_complete(uint16_t net_idx, uint16_t addr)
+{
+}
+
+static void prov_reset(void)
+{
+	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
+}
+
+static const uint8_t dev_uuid[16] = { 0xdd, 0xdd };
+
+static const struct bt_mesh_prov prov = {
+	.uuid = dev_uuid,
+	.output_size = 4,
+	.output_actions = BT_MESH_DISPLAY_NUMBER,
+	.output_number = output_number,
+	.complete = prov_complete,
+	.reset = prov_reset,
+};
+
+static void bt_ready(int err)
+{
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_mesh_init(&prov, &comp);
+	if (err) {
+		printk("Initializing mesh failed (err %d)\n", err);
+		return;
+	}
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	/* This will be a no-op if settings_load() loaded provisioning info */
+	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
+
+	printk("Mesh initialized\n");
+}
 
 static void test_power_down(void)
 {
-	zassert_true(0 == bt_mesh_init(NULL, &comp), "bt mesh initialization failed.");
+	int err = bt_enable(bt_ready);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+	}
 	
-	bt_mesh_power_down();
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+			settings_load();
+	}
+
+	bt_mesh_power_down_enter();
+	bt_mesh_power_down_leave();
 }
 
 void test_main(void)
 {
 	ztest_test_suite(scheduler_srv_test,
-			ztest_unit_test(test_scheduler_init),
 			ztest_unit_test(test_power_down)
 			 );
 
